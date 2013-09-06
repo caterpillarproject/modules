a86980564e01c199e3ace7d23910e948fa431a53
Brendan Griffen <projectcaterpillar@gmail.com>
2013-09-05 23:19:46 -0400

    added particle distribution analysis for ics

---
+++ partdist.py
@@ -0,0 +1,22 @@
+import readsnapshots.readsnapHDF5 as rs
+import pylab as plt
+import sys
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_ylabel("Number", fontsize=14)
+ax.set_xlabel("Particle Type", fontsize=14)
+snapshot = 63
+
+def pdist(file):
+    inputdir = sys.argv[0]
+    header = rs.snapshot_header(inputdir)
+    nall = np.log10(header.nall)
+    partypes = np.array([0,1,2,3,4,5])
+    ax.plot(partypes,nall,)
+    plt.show()
+
+if __name__ == '__main__':
+    import os
+    pdist(os.getcwd() + "/ics_rewrite.0")
