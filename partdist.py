2694572028fd918f6a172fc920422cc4d784d28d
bgriffen <brendan.f.griffen@gmail.com>
2013-09-12 15:38:29 -0400

    updated

--- partdist.py
+++ partdist.py
@@ -3,6 +3,7 @@ import pylab as plt
 import sys
 import numpy as np
 import os
+from operator import itemgetter
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -13,6 +14,8 @@ ax.set_xticklabels(('0','1','2','3','4','5'))
 ax.set_xticks((0,1,2,3,4,5))
 snapshot = 63
 
+pmgridlist = [128,256,512,1024]
+
 def pdist(filename):
     header = rs.snapshot_header(filename)
     title = os.getcwd().split(os.sep)[-1]
@@ -22,11 +25,19 @@ def pdist(filename):
     partypes = np.array([0,1,2,3,4,5])
     nlowres = nallnorm[2:5].sum()
     nhighres = nallnorm[1]
+    pmgridfloat = nlowres**(1./3.)*2.
+    #for pmgrid in pmgridlist:
+        #min(enumerate(a), key=itemgetter(1))[0]
+        #deltagrid.append(pmgridfloat - float(pmgrid))
+
+    deltagrid = abs(pmgridfloat - np.array(pmgridlist))
+    index = min(enumerate(deltagrid), key=itemgetter(1))[0]
     print "\n"
     print "# Run:",title
-    print "# low np:",nlowres
-    print "# high np:",nhighres
-    print "# Recommended approx. PMGRID:",nlowres**(1./3.)*2.
+    print "# Np low-res: ",nlowres
+    print "# Np high-res:",nhighres
+    print "# Exact PMGRID:",int(pmgridfloat)
+    print "# Use PMGRID:",pmgridlist[index]
     print "# Plotting..."
     ax.bar(partypes,nall,width=0.5,align='center')
     ax.set_title(title,fontsize=14)
